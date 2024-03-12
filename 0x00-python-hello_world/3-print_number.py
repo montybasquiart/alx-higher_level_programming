f72761d45d2d1dbe92e80140efa7362ddae7e5cb
montybasquiart <owamathias@gmail.com>
2024-03-12 14:51:49 +0000

    added comments to define my code appropriately

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number = 98
-print(f"{number:d} Battery street", end="\n")
+number = 98 # variable declaration
+print(f"{number:d} Battery street", end="\n") # print integer stored in the var
