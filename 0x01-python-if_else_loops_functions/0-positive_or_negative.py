b21b00e893ae4984a90b1ab3625b04024f1f83de
montybasquiart <owamathias@gmail.com>
2024-03-20 13:28:32 +0000

    updated a typo in my comments

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 
-# This program will asign a random signed number to the variable number
+# This program will assign a random signed number to the variable number
 # each time it is executed.
 if number > 0:
     print(f"{number} is positive", end="\n")
