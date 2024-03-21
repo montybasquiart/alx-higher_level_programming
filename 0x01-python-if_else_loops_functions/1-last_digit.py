1ffdcd5180323a0ce932cc3a30abf6c2a79c053a
montybasquiart <owamathias@gmail.com>
2024-03-21 01:49:17 +0000

    Fixed pycodestyle validation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,8 +13,10 @@ else:
 
 # Check conditions and print messages
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5", end="\n")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5"
+          , end="\n")
 if last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0", end="\n")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0"
+          , end="\n")
 if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0", end="\n")
