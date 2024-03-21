9f0daeffb91fe049bf8c8cfb09e273ffe7800a02
montybasquiart <owamathias@gmail.com>
2024-03-21 02:44:01 +0000

    A program that prints the ASCII alphabet, in lowercase

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def lowercase_alphabets():
+    """A function that prints ASCII alphabets in lowercase."""
+
+# A program that prints the ASCII alphabet, in lowercase
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    print(letter, end="")
+
+# Call the function to print the lowercase alphabet without newlines
+lowercase_alphabets()
