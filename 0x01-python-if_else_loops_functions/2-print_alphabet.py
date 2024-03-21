32b79a7430f756fc7f79e57b090fb21f45c873d5
montybasquiart <owamathias@gmail.com>
2024-03-21 03:02:40 +0000

    Fixed code output error and pycodestyle validation

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,9 +2,9 @@
 def lowercase_alphabets():
     """A function that prints ASCII alphabets in lowercase."""
 
+
 # A program that prints the ASCII alphabet, in lowercase
 for letter in "abcdefghijklmnopqrstuvwxyz":
-    print(letter.format("letter"))
-
+    print(letter.format("letter"), end="")
 # Call the function to print the lowercase alphabet without newlines
 lowercase_alphabets()
