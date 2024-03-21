c3cdcaa33b71ed4e3746ef3bab4c8ae9afbc0341
montybasquiart <owamathias@gmail.com>
2024-03-21 13:11:17 +0000

    updated program to print ASCII alphabet except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -6,4 +6,11 @@ def lowercase_alphabets():
 
 # A program that prints the ASCII alphabet, in lowercase,
 # not followed by a new line.
-# Print all the letters except q and e
+# Print all the letters except "q" and "e"
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    if letter != 'q' and letter != 'e':
+        print(letter.format("letter"), end="")
+
+# Call the the function to print the lowercase alphabet
+# exclusding "q" and "e"
+lowercase_alphabets()
