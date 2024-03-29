95cefac6bba6501375f5978ca66a37faa3be3a16
montybasquiart <owamathias@gmail.com>
2024-03-29 23:56:13 +0000

    my code is correct but keep getting errors

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     """A Function that prints a string in UPPERCASE."""
-    result = ""
     for char in str:
-        if 97 <= ord(char) <= 122:
-            upper_char = chr(ord(char) - 32)
-            result += upper_char
-        else:
-            result += char
-    print("{}".format(result))
-
-
-
-    #for char in str:
-     #   if ord(char) >= 97 and ord(char) <= 122:
-      #      char = chr(ord(char) - 32)
-       # print("{}".format(char), end="")
-    #print()
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()
