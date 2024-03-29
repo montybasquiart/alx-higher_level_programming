19adb8ef508acee43ee16fe41ae3f3007a642c03
montybasquiart <owamathias@gmail.com>
2024-03-29 18:39:15 +0000

    updated code ouput error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """A Function that prints a string in UPPERCASE.
-    """
+    """A Function that prints a string in UPPERCASE."""
     for char in range(len(str)):
         if ord(str[char]) >= 97 and ord(str[char]) < 123:
             letter = 32
