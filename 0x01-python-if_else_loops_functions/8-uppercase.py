79ac82f57546016fec61501e0e33eaac5af02138
montybasquiart <owamathias@gmail.com>
2024-03-30 00:14:27 +0000

    added function descrition to my code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     """A function that prints in UPPERCASE."""
-    for char in str:
-        value = ord(char)
+    for i in str:
+        value = ord(i)
         if value > 96 and value < 123:
             value -= 32
         print("{:c}".format(value), end="")
