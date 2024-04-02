935e02a57aacf00f4c73bb54af3a1513cfa8cd29
montybasquiart <owamathias@gmail.com>
2024-04-02 22:50:53 +0000

    corrected pycodestyle validation error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,6 +4,6 @@
 # alternating lowercase and uppercase(z in lowercase Y in uppercase).
 
 i = 0
-for char in range(ord("z"), ord("a") -1, -1):
+for char in range(ord("z"), ord("a") - 1, - 1):
     print("{}".format(chr(char - i)), end="")
     i = 32 if i == 0 else 0
