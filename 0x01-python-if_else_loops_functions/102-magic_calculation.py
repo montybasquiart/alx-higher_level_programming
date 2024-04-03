e44e9bc1cc0735accfcecae31cacca3ee9f626d2
montybasquiart <owamathias@gmail.com>
2024-04-03 01:07:09 +0000

    a function that prints Python bytecode

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    """A function that prints Python bytecode.
+    """
+    if a < b:
+        return c
+    elif b > c:
+        return a + b
+    else:
+        return a * b - c
