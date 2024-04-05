c8c1489cce7909c401128f17712a2ec9ac00e2f9
montybasquiart <owamathias@gmail.com>
2024-04-05 04:26:59 +0000

    pep8 says line of code in line 8 was too long so i shortened the variable name

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    addition = add(a, b)
-    print("{a} + {b} = {addition}".format(a=a, b=b, addition=addition), end="\n")
+    add_it = add(a, b)
+    print("{a} + {b} = {add_it}".format(a=a, b=b, add_it=add_it), end="\n")
