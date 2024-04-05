ce4efecc2bf4074534e5971fd9c0049669c22f23
montybasquiart <owamathias@gmail.com>
2024-04-05 04:17:56 +0000

    fixed code output error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-from add_0 import add
-
-a = 1
-b = 2
-addition = add(a, b)
-print("{a} + {b} = {addition}".format(a=a, b=b, addition=addition), end="\n")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    addition = add(a, b)
+    print("{a} + {b} = {addition}".format(a=a, b=b, addition=addition), end="\n")
