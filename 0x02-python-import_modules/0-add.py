89151ec794dd1c4d4bfd7eaa282667628d600f24
montybasquiart <owamathias@gmail.com>
2024-04-07 22:16:49 +0000

    Fixed pycodestyle requirement error and also updated import modules that were moved into tests folder

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from add_0 import add
+    from tests import add_0
     a = 1
     b = 2
-    add_it = add(a, b)
+    add_it = add_0.add(a, b)
     print("{a} + {b} = {add_it}".format(a=a, b=b, add_it=add_it), end="\n")
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,22 +2,22 @@
 
 if __name__ == "__main__":
     # This program imports functions and does some basic Maths
-    import calculator_1 as cal
+    from tests import calculator_1 as cal
     a = 10
     b = 5
 
     # This program does Addition
-    addition = cal.add(a, b)
-    print("{a} + {b} = {addition}".format(a=a, b=b, addition=addition), end="\n")
+    add_it = cal.add(a, b)
+    print("{a} + {b} = {add_it}".format(a=a, b=b, add_it=add_it), end="\n")
 
     # This program does Subtraction
-    subtract = cal.sub(a, b)
-    print("{a} - {b} = {subtract}".format(a=a, b=b, subtract=subtract), end="\n")
+    sub_it = cal.sub(a, b)
+    print("{a} - {b} = {sub_it}".format(a=a, b=b, sub_it=sub_it), end="\n")
 
     # This program does Multiplication
-    multiply = cal.mul(a, b)
-    print("{a} * {b} = {multiply}".format(a=a, b=b, multiply=multiply), end="\n")
+    mul_it = cal.mul(a, b)
+    print("{a} * {b} = {mul_it}".format(a=a, b=b, mul_it=mul_it), end="\n")
 
     # This program does Division
-    divide = cal.div(a, b)
-    print("{a} / {b} = {divide}".format(a=a, b=b, divide=divide), end="\n")
+    div_it = cal.div(a, b)
+    print("{a} / {b} = {div_it}".format(a=a, b=b, div_it=div_it), end="\n")
