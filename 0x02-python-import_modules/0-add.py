a91703653a133472530345f0c5dfd0258ba20978
montybasquiart <owamathias@gmail.com>
2024-04-07 22:33:52 +0000

    updated code output error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import add_0
+    import add_0 as addition
     a = 1
     b = 2
-    add_it = add_0.add(a, b)
+    add_it = addition.add(a, b)
     print("{a} + {b} = {add_it}".format(a=a, b=b, add_it=add_it), end="\n")
