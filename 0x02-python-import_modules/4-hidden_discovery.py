e09eff8d9fc5d5b372e4def292f3deb158f2a49f
montybasquiart <owamathias@gmail.com>
2024-04-08 10:48:54 +0000

    changed module import format

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__":
     """A program that prints all names defined by the compiled module.
     """
-    import hidden_4
+    import hidden_4 as hidden_names
 
-    names = dir(hidden_4)
+    names = dir(hidden_names)
     names.sort()
     for name in names:
         if not name.startswith("__"):
