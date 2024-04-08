caabe45af68b8e987a581ea3ff3a0a2e68f285f8
montybasquiart <owamathias@gmail.com>
2024-04-08 11:10:34 +0000

    a program that imports and prints the value of \'a\'

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """A program that imports and prints the variable "a".
+    """
+    from variable_load_5 import a
+
+    # Print the value of "a"
+    print(a)
