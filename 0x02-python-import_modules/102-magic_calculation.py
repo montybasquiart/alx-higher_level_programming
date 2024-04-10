84057a5aea12f4fcd9fb2f300d67f6324722a365
montybasquiart <owamathias@gmail.com>
2024-04-10 12:40:31 +0000

    Fixed code output error where import module was not present

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,8 +2,7 @@
 def magic_calculation(a, b):
     """A program that does some Python magic.
     """
-    from magic_calculation import add, sub
-    
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
