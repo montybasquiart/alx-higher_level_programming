e584b2224b3bd602bb39a8f0f915a37dc07ac6db
montybasquiart <owamathias@gmail.com>
2024-04-11 04:59:52 +0000

    I was initializing my_list inside the function as a result my code keeps failing

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,5 @@
 def print_list_integer(my_list=[]):
     """A function that prints all integers of a list.
     """
-    my_list = [1, 2, 3, 4, 5]
-
     for i in my_list:
         print("{i}".format(i=i), end="\n")
