4cd99345d4f13d5ac99efe4e267ba96a86631018
montybasquiart <owamathias@gmail.com>
2024-04-11 04:53:45 +0000

    Fixed code ouput error where i tried a for loop

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,8 +4,5 @@ def print_list_integer(my_list=[]):
     """
     my_list = [1, 2, 3, 4, 5]
 
-    print("{my_list[0]}".format(my_list=my_list), end="\n")
-    print("{my_list[1]}".format(my_list=my_list), end="\n")
-    print("{my_list[2]}".format(my_list=my_list), end="\n")
-    print("{my_list[3]}".format(my_list=my_list), end="\n")
-    print("{my_list[4]}".format(my_list=my_list), end="\n")
+    for i in my_list:
+        print("{i}".format(i=i), end="\n")
