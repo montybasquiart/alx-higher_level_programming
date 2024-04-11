70507411e5137632b7bb6f2c21d33745344eb297
montybasquiart <owamathias@gmail.com>
2024-04-11 04:49:41 +0000

    Fixed code ouput error

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,9 @@ def print_list_integer(my_list=[]):
     """A function that prints all integers of a list.
     """
     my_list = [1, 2, 3, 4, 5]
-    print(my_list[0])
-    print(my_list[1])
-    print(my_list[2])
-    print(my_list[3])
-    print(my_list[4])
+
+    print("{my_list[0]}".format(my_list=my_list), end="\n")
+    print("{my_list[1]}".format(my_list=my_list), end="\n")
+    print("{my_list[2]}".format(my_list=my_list), end="\n")
+    print("{my_list[3]}".format(my_list=my_list), end="\n")
+    print("{my_list[4]}".format(my_list=my_list), end="\n")
