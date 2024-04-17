1b9d719abcdbe963809e22798395560d9240a4c2
montybasquiart <owamathias@gmail.com>
2024-04-17 03:33:28 +0000

    Updated brackets around return statements

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,8 +4,8 @@ def new_in_list(my_list, idx, element):
     without modifying the original list.
     """
     if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
+        return (my_list.copy())
     else:
         new_list = my_list.copy()
         new_list[idx] = element
-        return new_list
+        return (new_list)
