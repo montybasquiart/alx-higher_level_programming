dfcbe76a35d1f460eb72bca2b2f770037f6eddeb
montybasquiart <owamathias@gmail.com>
2024-04-17 01:18:48 +0000

    Fixed code output error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     """A function that prints all integers of a list, in a reverse order.
     """
-    for number in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[number]))
+    for number in reversed(my_list):
+        print("{:d}".format(number))
