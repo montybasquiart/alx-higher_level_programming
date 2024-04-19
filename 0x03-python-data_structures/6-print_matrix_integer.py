8b30d0e1da547156bd16e2b74d1fbf503b7e0219
montybasquiart <owamathias@gmail.com>
2024-04-19 00:29:00 +0000

    fixed code output error where pipping wasnt working properly

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,4 +7,4 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}".format(value), end="")
             if i < len(row) - 1:
                 print(" ", end="")
-        print("$", " ")
+        print("")
