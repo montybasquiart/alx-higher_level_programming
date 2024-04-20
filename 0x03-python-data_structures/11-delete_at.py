84857877089ea983e6aebffdc9926212412eb0e1
montybasquiart <owamathias@gmail.com>
2024-04-20 20:46:07 +0000

    Fixed code output error where I was appending new_list to my_list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,6 +6,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return(my_list)
     else:
-        new_list.append(my_list)
+        new_list.extend(my_list[:idx])
+        new_list.extend(my_list[idx + 1:])
         del my_list[idx]
-        return (my_list)
+        return (new_list)
