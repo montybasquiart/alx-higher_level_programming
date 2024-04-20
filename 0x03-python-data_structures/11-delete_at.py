0d9982af0cb7b3238852bf137ac6594c7a818169
montybasquiart <owamathias@gmail.com>
2024-04-20 20:39:47 +0000

    Fixed code output error

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,10 @@
 def delete_at(my_list=[], idx=0):
     """A function that deletes the item at a specific position in a list.
     """
+    new_list = []
     if idx < 0 or idx >= len(my_list):
         return(my_list)
     else:
-        new_list = my_list
+        new_list.append(my_list)
         del my_list[idx]
-        return (new_list)
+        return (my_list)
