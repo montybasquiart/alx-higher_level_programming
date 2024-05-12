f19d2d41624d15445c7ca4f1f4fa1e41f2c7bbb5
montybasquiart <owamathias@gmail.com>
2024-05-12 23:06:29 +0000

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """A function that replaces all occurences of an element by another in a new list.
+    """
+    new_list = []
+    for numbers in my_list:
+        if numbers == search:
+            new_list.append(replace)
+        else:
+            new_list.append(numbers)
+    return new_list
