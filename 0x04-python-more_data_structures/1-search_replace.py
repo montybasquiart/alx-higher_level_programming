a9458bc4b1cee9d6d85dec518306ed08451d5346
Mathias Owa Martins <owamathias@gmail.com>
2024-05-13 09:13:21 +0100

    Fixed code output error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,10 +3,4 @@ def search_replace(my_list, search, replace):
     """A function that replaces all occurrences of an element by another in a new
     list.
     """
-    new_list = []
-    for x in my_list:
-        if x == search:
-            new_list.append(replace)
-        else:
-            new_list.append(x)
-    return new_list
+    return [numbers if not numbers == search else replace for numbers in my_list]
