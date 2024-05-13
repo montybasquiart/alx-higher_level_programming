37d12551d97ea19775d6e26c6969edd88aca408d
montybasquiart <owamathias@gmail.com>
2024-05-13 00:00:43 +0000

    Fixed code output error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     """A function that replaces all occurences of an element by another in a new
     list.
     """
-    replaced_list = [0 for i in range(len(my_list))]
+    replaced_list = [0 for numbers in range(len(my_list))]
     for numbers in range(len(my_list)):
         if my_list[numbers] == search:
             replaced_list[numbers] = replace
