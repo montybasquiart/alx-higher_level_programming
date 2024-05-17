26c1fd076925d2f6f95d77985091523c1ef0afbb
Mathias Owa Martins <owamathias@gmail.com>
2024-05-17 15:17:40 +0100

    Fixed code output error

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    """A function that adds all unique integers in a list (only once for each integer).
+    """A function that adds all unique integers in a list
+    (only once for each integer).
     """
     unique_int = set(my_list)
     add_result = sum(unique_int)
