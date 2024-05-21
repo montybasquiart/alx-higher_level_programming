ba5b0e8bd5ec8e402ac1b6e260fd865a5f06b075
Mathias Owa Martins <owamathias@gmail.com>
2024-05-21 23:25:41 +0100

    Fixed code output error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     """A function that prints a dictionary by ordered keys
     """
     for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key])) 
-        
+        print("{}: {}".format(key, a_dictionary[key]))
