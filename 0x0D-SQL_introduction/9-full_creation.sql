e26df53cedd611190b48a514d5f0a6e3605dbd50
Mathias Owa Martins <owamathias@gmail.com>
2024-05-23 01:34:05 +0100

    Fixed code output error

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Creates a table second_table in the database.
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` VALUES 
+INSERT INTO `second_table` (`id`, `name`, `score1`) VALUES 
 (`id` 1, `name` "John", `score` 10),
 (`id` 2, `name` "Alex", `score` 3),
 (`id` 3, `name` "Bob", `score` 14),
