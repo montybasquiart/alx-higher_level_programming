b0c17161dd6d466f561584ebb02bba47dff7c7ca
Mathias Owa Martins <owamathias@gmail.com>
2024-05-23 03:42:50 +0100

    Fixed code output error

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Displays the max temperature of each state, ordered by state name.
-SELECT `city`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state`;
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state`;
