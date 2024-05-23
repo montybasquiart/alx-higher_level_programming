555d3645e0d39a1bb8c63f892828f6f667f7b837
Mathias Owa Martins <owamathias@gmail.com>
2024-05-23 01:50:03 +0100

    Fixed code output error

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Lists all the records of the table second_table of the database.
-SELECT `score` `name` FROM `second_table` ORDER BY `score` DESC; -- Records are ordered by score (top first)
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC; -- Records are ordered by score (top first)
