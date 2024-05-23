b0d05ea40db5385e5b245e8ac5600e5ff4dfa326
Mathias Owa Martins <owamathias@gmail.com>
2024-05-23 02:27:38 +0100

    Fixed code output error

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- Updates the score of Bob to 10 in the table second_table.
-UPDATE `second_table` WHERE `name` "Bob" SET `score` = 10; -- Updates Bob's score from 14 to 10, using name field
+UPDATE `second_table` WHERE `name` = "Bob" SET `score` = 10; -- Updates Bob's score from 14 to 10, using name field
