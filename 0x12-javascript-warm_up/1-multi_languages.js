ac1f77cbe1f9e7a9b4aa5debe039b051b8e07d5b
Mathias Owa Martins <owamathias@gmail.com>
2024-06-20 12:53:46 +0100

    Fixed code output error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // A script that prints 3 lines
-// First line
+
 console.log('C is fun');
-// Second line
 console.log('Python is cool');
-// Third line
 console.log('Javascript is amazing');
