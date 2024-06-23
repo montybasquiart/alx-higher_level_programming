8dfde0a4dbf2d254eebd2322ed76ec9905a7ac5d
Mathias Owa Martins <owamathias@gmail.com>
2024-06-24 00:50:18 +0100

    A script that prints the first argument converted in integer if the first argument can be converted to an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* A script that prints the first argument converted in integer if the first
+argument can be converted to an integer
+*/
+
+const arg = process.argv[2];
+const convertedNumbers = Number(arg);
+
+if (!isNaN(convertedNumbers)) {
+  console.log('My number:', convertedNumbers);
+} else {
+  console.log('Not a number');
+}
