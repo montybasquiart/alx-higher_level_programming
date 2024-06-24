b871be12c992b19b625ee6bffe0fcdb58cf12eae
Mathias Owa Martins <owamathias@gmail.com>
2024-06-24 02:25:51 +0100

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* A script that prints 3 lines by using an array of string and a loop.
+*/
+const threeLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let index = 0; index < threeLines.length; index++) {
+  console.log(threeLines[index]);
+}
