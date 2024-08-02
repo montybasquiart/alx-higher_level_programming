fb9383865cdf2e70b7cafd819e38844d84f07ce9
montybasquiart <owamathias@gmail.com>
2024-08-02 15:42:01 +0100

    A function that returns a key with the biggest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,9 +5,7 @@ def best_score(a_dictionary):
 
     if not a_dictionary:
         return None
-    
     max_value = max(a_dictionary.values())
-    
     for key in a_dictionary:
         if a_dictionary[key] == max_value:
             return key
