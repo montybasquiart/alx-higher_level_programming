9855938540c4c9cc8745583a506fd6ffcaea709f
Mathias Owa Martins <owamathias@gmail.com>
2024-08-02 12:53:33 +0100

    fixed line too long

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    """A function that returns a new dictionary with all values multiplied by 2.
+    """A function that returns a new dictionary with 
+    all values multiplied by 2.
     """
     new_dict = a_dictionary.copy()
     for key in new_dict:
