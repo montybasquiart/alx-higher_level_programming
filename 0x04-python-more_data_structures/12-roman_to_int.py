cfd12e3c6433689af540536d8eb7dd21c50f43de
Mathias Owa Martins <owamathias@gmail.com>
2024-08-03 01:05:27 +0100

    Fixed no new line at the end of file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,13 +7,13 @@ def roman_to_int(roman_string):
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    old_value = 0
+    previous_char = 0
 
     for char in roman_string:
         current_value = roman_dict[char]
-        if current_value > old_value:
-            num += current_value - 2 * old_value
+        if current_value > previous_char:
+            num += current_value - 2 * previous_char
         else:
             num += current_value
-        old_value = current_value
+        previous_char = current_value
     return num
