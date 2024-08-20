32e8902b8711840180c89e7229bb8f40b64c1fc7
Mathias Owa Martins <owamathias@gmail.com>
2024-08-20 18:32:07 +0100

    A function that prints an integer with {:d}.format().

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """A function that prints an integer with "{:d}".format().
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
+    
