07b6560cf51fc4326561aad38b38fea07c0cddbe
Mathias Owa Martins <owamathias@gmail.com>
2024-08-20 18:45:14 +0100

    Fixed code output error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     """A function that prints an integer with "{:d}".format().
     """
     try:
-        print("{:d}".format(value), end="")
+        print("{:d}".format(value))
         return True
     except:
         return False
