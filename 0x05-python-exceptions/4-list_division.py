0d95697a49451c31cba0a78dabd3498fb148dc0a
Mathias Owa Martins <owamathias@gmail.com>
2024-08-21 17:45:00 +0100

    Fixed code output error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,20 +5,16 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for index in range(list_length):
         try:
-            if index >= len(my_list_1) or index >= len(my_list_2):
-                print("out of range")
-                result.append(0)
-                continue
-
             dividend = my_list_1[index]
             divisor = my_list_2[index]
             result.append(dividend / divisor)
-
+        except IndexError:
+            print("out of range")
+            result.append(0)
         except TypeError:
             print("wrong type")
             result.append(0)
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
-
     return result
