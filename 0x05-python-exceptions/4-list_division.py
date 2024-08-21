833f0302b7a8e75150e33d0d2d8ab599b435feeb
Mathias Owa Martins <owamathias@gmail.com>
2024-08-21 17:40:38 +0100

    Fixed code output error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,16 +9,16 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("out of range")
                 result.append(0)
                 continue
-            
-            dividend = my_list_1[index]  
-            divisor = my_list_2[index]  
-            result.append(dividend / divisor)  
 
-        except TypeError:  
-            print("wrong type")  
-            result.append(0)  
-        except ZeroDivisionError:  
-            print("division by 0")  
-            result.append(0)  
+            dividend = my_list_1[index]
+            divisor = my_list_2[index]
+            result.append(dividend / divisor)
 
-    return result  
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+
+    return result
