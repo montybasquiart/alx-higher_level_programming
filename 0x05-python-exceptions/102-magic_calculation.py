a373d41a7c61b9df6c5585e2abdb0943ee24ff86
Mathias Owa Martins <owamathias@gmail.com>
2024-08-21 19:07:55 +0100

    A python function that performs a bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for index in range(1, 4):
+        if index > a:
+            raise Exception("Too far")
+        result += (a ** b) / index
+    result += a + b
+    return
